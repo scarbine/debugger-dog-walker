2ce7f15ff0d9c7125e5178ba26cf914bfabe4149
Sam Carbine <scarbine@me.com>
2021-06-02 13:45:40 -0500

    added onClick event to walker

--- scripts/RegisteredPets.js
+++ scripts/RegisteredPets.js
@@ -1,12 +1,28 @@
 import { getPets } from "./database.js"
 
+// document.addEventListener(
+//     "click",
+//     (clickEvent) => {
+//         const itemClicked = clickEvent.target
+//         if (itemClicked.id.startsWith("pets")) {
+//             const [,petId] = itemClicked.id.split("--")
+
+//             for (const pet of pets) {
+//                 if (pet.id === parseInt(petId)) {
+//                     window.alert(`${pet.name} live in ${pet.city}`)
+//                 }
+//             }
+//         }
+//     }
+// )
+
 const pets = getPets()
 
 export const RegisteredPets = () => {
     let petHTML = "<ul>"
 
     for (const pet of pets) {
-        petHTML += `<li>${pet.name}</li>`
+        petHTML += `<li id="pets--${pet.id}">${pet.name}</li>`
     }
 
     petHTML += "</ul>"
